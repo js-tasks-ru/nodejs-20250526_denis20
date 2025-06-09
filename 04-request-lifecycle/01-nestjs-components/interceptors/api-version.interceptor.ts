f15f80c86be6482df915af27f60826ceb47fdc13
Рамзин Денис <d.ramzin@dhabits.ru>
2025-06-09 17:26:31 +0300

    Задача 4. Компоненты Nest

--- 04-request-lifecycle/01-nestjs-components/interceptors/api-version.interceptor.ts
+++ 04-request-lifecycle/01-nestjs-components/interceptors/api-version.interceptor.ts
@@ -1,5 +1,16 @@
 import { NestInterceptor, ExecutionContext, CallHandler } from "@nestjs/common";
+import { map, tap } from "rxjs";
 
 export class ApiVersionInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler) {}
+  intercept(context: ExecutionContext, next: CallHandler) {
+    const now = Date.now();
+
+    return next.handle().pipe(
+      map((data) => ({
+        ...data,
+        apiVersion: "1.0",
+        executionTime: `${Date.now() - now}ms`,
+      })),
+    );
+  }
 }
